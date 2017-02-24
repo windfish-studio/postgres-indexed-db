e0e7a8f1050f85e7498b824a296158839dbbbb90
Alex Pilafian <sikanrong@Alexs-MacBook-Pro.local>
2017-02-24 22:43:11 +0100

    Finishing first and most central test (could still use some polishing). Adding manifest to exporter output with table names and row_count for each table. Removes row_count from output filenames as _manifest is an arguably cleaner solution.

--- test/test.js
+++ test/test.js
@@ -8,29 +8,63 @@ var _ = require('lodash');
 var pg = require('pg');
 var exporter = require('../lib/index.js').exporter;
 var tape = require('tape');
+var fs = require('fs');
+var path = require('path');
+
+var conf = {
+    db: {
+        database: "dvdrental_sample"
+    },
+    output_path: "test/export_data",
+    results_per_page: 100
+};
 
 tape("should export all database data to specified directory", function (t) {
-    var observable = exporter({
-        db: {
-            database: "dvdrental_sample"
-        },
-        output_path: "test/export_data",
-        results_per_page: 100
-    });
 
-    observable.subscribe(function(msg_o){
-        switch(msg_o.message){
-            case "finished":
-                t.end();
-                console.log('finished');
-                break;
-            case "table_finished":
-                console.log('\''+msg_o.value+'\' write finished...');
-                break;
-            case "progress":
-                console.log("%"+(msg_o.value*100).toFixed(2)+" done...");
-                break;
-        }
+    var observable = exporter(conf);
+
+    var pool = new pg.Pool(conf.db);
+    pool.connect(function (err, client, done){
+
+        observable.subscribe(function(msg_o){
+            switch(msg_o.message){
+                case "finished":
+                    console.log('finished');
+
+                    var output_filenames = fs.readdirSync(path.join(__dirname, './export_data'));
+                    var manifest = require(path.join(__dirname, 'export_data', '_manifest.json'));
+
+                    //get tables data from database
+                    pool.query("SELECT table_name, table_schema FROM information_schema.tables", []).then(function(result){
+                        var db_tables = [];
+                        _.each(result.rows, function(row){
+                            if(row.table_schema == 'public'){
+                                db_tables.push(row.table_name);
+                                t.equal(typeof manifest[row.table_name], 'object'); //manifest entry should exist for each table
+                            }
+                        });
+
+                        t.equals(output_filenames.length - 1, db_tables.length); //minus one for manifest
+
+                        _.each(db_tables, function(table_name){
+
+                            var output_ar = require(path.join(__dirname, 'export_data', table_name+'.json'));
+                            t.equal(manifest[table_name].row_count, output_ar.length);
+                        });
+
+                        done();
+                        pool.end();
+
+                        t.end();
+                    });
+
+                    break;
+                case "progress":
+                    console.log("%"+(msg_o.value*100).toFixed(2)+" done...");
+                    break;
+            }
+
+        });
 
     });
 });
