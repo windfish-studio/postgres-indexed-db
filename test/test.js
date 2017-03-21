4ef881d26e63e0a802b2bcc5151649e960ca9367
Alex Pilafian <sikanrong@Alexs-MacBook-Pro.local>
2017-03-21 14:18:36 +0100

    cleans up exporter.js test (test.js). Simplifies table_finished callbacks and closures. Still work to be done to make sure exporter progress reporting is accurate.

--- test/test.js
+++ test/test.js
@@ -9,6 +9,7 @@ var pg = require('pg');
 var exporter = require('../lib/index.js');
 var tape = require('tape');
 var fs = require('fs');
+var q = require('q');
 var path = require('path');
 
 var conf = {
@@ -19,52 +20,74 @@ var conf = {
     results_per_page: 100
 };
 
-tape("should export all database data to specified directory", function (t) {
+var export_promise = do_export();
+var db_schema_promise = get_db_schema();
 
-    var observable = exporter(conf);
+var db_schema;
 
-    var pool = new pg.Pool(conf.db);
-    pool.connect(function (err, client, done){
+db_schema_promise.then(function (result) {
+    db_schema = result.rows;
+});
 
-        observable.subscribe(function(msg_o){
-            switch(msg_o.message){
-                case "finished":
-                    console.log('finished');
+q.all([export_promise, db_schema_promise]).then(function () {
 
-                    var output_filenames = fs.readdirSync(path.join(__dirname, './export_data'));
-                    var manifest = require(path.join(__dirname, 'export_data', '_manifest.json'));
+    var output_filenames = fs.readdirSync(path.join(__dirname, './export_data'));
+    var manifest = require(path.join(__dirname, 'export_data', '_manifest.json'));
 
-                    //get tables data from database
-                    pool.query("SELECT table_name, table_schema, table_type FROM information_schema.tables", []).then(function(result){
-                        var db_tables = [];
-                        _.each(result.rows, function(row){
-                            if(row.table_schema == 'public' && row.table_type == 'BASE TABLE'){
-                                db_tables.push(row.table_name);
-                                t.equal(typeof manifest[row.table_name], 'object'); //manifest entry should exist for each table
-                            }
-                        });
+    tape("should export all database data in JSON format", function (t) {
+        //get tables data from database
 
-                        t.equals(output_filenames.length - 1, db_tables.length); //minus one for manifest
-
-                        _.each(db_tables, function(table_name){
-                            var output_ar = require(path.join(__dirname, 'export_data', table_name+'.json'));
-                            t.equal(manifest[table_name].row_count, output_ar.length);
-                        });
-
-                        done();
-                        pool.end();
-
-                        t.end();
-                    });
-
-                    break;
-                case "progress":
-                    console.log("%"+(msg_o.value*100).toFixed(2)+" done...");
-                    break;
+        var db_tables = [];
+        _.each(db_schema, function (row) {
+            if (row.table_schema == 'public' && row.table_type == 'BASE TABLE') {
+                db_tables.push(row.table_name);
+                t.equal(typeof manifest[row.table_name], 'object'); //manifest entry should exist for each table
             }
-
         });
 
+        t.equals(output_filenames.length - 1, db_tables.length); //minus one for manifest
+
+        _.each(db_tables, function (table_name) {
+            var output_ar = require(path.join(__dirname, 'export_data', table_name + '.json'));
+            t.equal(manifest[table_name].row_count, output_ar.length);
+        });
+
+        t.end();
     });
 });
 
+function get_db_schema () {
+    var def = q.defer();
+    var pool = new pg.Pool(conf.db);
+    pool.connect(function (err, client, done) {
+        pool.query("SELECT table_name, table_schema, table_type FROM information_schema.tables", []).then(function (result) {
+            def.resolve(result);
+            done();
+            pool.end();
+        });
+
+    });
+    return def.promise;
+};
+
+function do_export () {
+
+    var observable = exporter(conf);
+
+    var def = q.defer();
+
+    observable.subscribe(function(msg_o){
+        switch(msg_o.message){
+            case "finished":
+                console.log('finished');
+                def.resolve();
+                break;
+            case "progress":
+                console.log("%"+(msg_o.value*100).toFixed(2)+" done...");
+                break;
+        }
+
+    });
+
+    return def.promise;
+};
